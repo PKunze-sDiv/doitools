3f492a83f8eb035f03dff776840c3dbcbf2fb754
Kunze <pk61pixu@idiv.de>
2022-09-21 12:33:16 +0200

    Initial commit for doitools

---
+++ R/altmetric.R
@@ -0,0 +1,340 @@
+#
+# A set of functions providing easy access to Altmetric publication data
+#
+
+
+
+
+
+argument_validity_check <- function(doi_vec, api_key = NULL, wait_for = 1) {
+
+    if (!is.character(doi_vec)) stop("Argument doi_vec must be a character vector.")
+    if (length(doi_vec) == 0)   stop("Argument doi_vec must not be of length 0.")
+    if (!is.null(api_key) &&
+        !is.character(api_key)) stop("Argument api_key must be NULL or a character vector.")
+    if (!is.null(api_key) &&
+        length(api_key) == 0)   stop("Argument api_key must be either NULL or of length greater than 0.")
+    if (!is.null(api_key) &&
+        length(api_key) > 1)    warning("Length of argument api_key greater than 1. Only first element used.")
+    if (!is.numeric(wait_for))  stop("Argument wait_for must be numeric.")
+    if (length(wait_for) == 0)  stop("Argument wait_for must not be of length 0.")
+    if (length(wait_for) > 1)   warning("Length of argument wait_for greater than 1. Only first element used.")
+
+}
+
+
+
+
+# Prints a message indicating the remaining calls before the rate limit is reached
+msg_limit_remainder <- function(api_info) {
+
+  message(
+      "API calls remaining:\n  this hour ",
+      (dplyr::pull(api_info, .data$remaining_calls_this_hour))[1],
+      "\n  today    ",
+      (dplyr::pull(api_info, .data$remaining_calls_today))[1]
+  )
+
+}
+
+
+
+
+# Returns the description for a given Altmetric API response status code
+altmetric_status_code_description <- function(status_code) {
+
+  if (!is.integer(status_code)) stop("Argument status_code must be of type integer.")
+  if (length(status_code) == 0) stop("Argument status_code must not be of length 0.")
+  if (length(status_code) > 1)  warning("Length of argument status_code greater than 1. Only first element used.")
+
+  switch(
+    as.character(status_code[1]),
+    "401" = "Invalid API key used.",
+    "403" = stringr::str_c("You aren't authorized for this call. Some calls and query types can only be ",
+                  "made by holders of an API key and/or a license for the full access version of the API."),
+    "404" = "Altmetric doesn't have any details for the DOI you provided.",
+    "429" = "Maximum number of calls exceeded. You are being rate limited.",
+    "502" = "The Altmetric Details Page API version you are using is currently down for maintenance.",
+    "Unkown status code"
+  )
+
+}
+
+
+
+
+# Turns the server response of the Altmetric API from a list into a tibble
+#' @importFrom rlang .data
+altmetric_response_to_tibble <- function(alt_list) {
+
+  if (!is.list(alt_list))    stop("Argument alt_list must be a list returned by the Altmetric API.")
+  if (length(alt_list) == 0) stop("Argument alt_list must not be of length 0.")
+
+  # the API response is a list that includes sublists which prevents the simple usage of as_tibble
+  # find out sublists and single elements
+  sublist_finder        <- purrr::map_lgl(alt_list, is.list)
+  names_sublists        <- names(sublist_finder[sublist_finder == TRUE])
+  names_single_elememts <- names(sublist_finder[sublist_finder == FALSE])
+
+  # put sublists in list columns inside of a tibble
+  sublists_tibble <- tibble::as_tibble(t(alt_list[names_sublists]))
+
+  # add residual single elements in individual columns
+  result_tibble <- tibble::as_tibble(alt_list[names_single_elememts])
+  result_tibble <- dplyr::bind_cols(result_tibble, sublists_tibble)
+
+  # refinement, needed due to inconsistency of Altmetric API responses
+  dplyr::mutate(result_tibble, last_updated = ifelse(is.character(.data$last_updated),
+                                                     readr::parse_integer(.data$last_updated),
+                                                     .data$last_updated))
+
+}
+
+
+
+
+#' Fetch publication data from the Altmetric API
+#'
+#' @description The Altmetric API provides a set of publication meta data including attention scores in social networks
+#' for a large number of publications. See [https://api.altmetric.com](https://api.altmetric.com) for more
+#' information. Use these functions to retrieve Altmetric data by supplying the DOI (digital object identifier)
+#' of a publication.
+#'
+#' @details
+#' \itemize{
+#'   \item `doi_get_altmetrics` is vectorized over doi_vec and allows downloading metrics of multiple publications. It
+#' provides a simple text progress bar.
+#'   \item `doi_get_altmetrics_single` fetches metrics for a single publication only. It is suited for non-interactive
+#' sessions like a shiny app that implements a custom progress indicator.
+#'   \item `doi_get_altmetrics_background` is suited for background jobs. It allows downloading a large number of
+#' publications that usually would exceed the API's rate limits. If the rate limit is reached, it waits
+#' until retrieval is possible again and then continues downloading until completion. Intermediate results will be
+#' saved in a file and can be backed up if the execution is interrupted.
+#' }
+#' @param doi_vec A character vector containing publication DOIs. doi_get_altmetrics_single only uses the
+#' first element.
+#' @param api_key The Altmetric api key to be used for the query.
+#' If no api key is provided, the access is currently limited to 720 calls per hour and 1200 calls per day.
+#' @param wait_for Time to wait between each api call in seconds. The default value is 1 which is what Altmetric
+#' urges you not to undercut. doi_get_altmetrics_background uses this as a fixed value.
+#' @return A list of two tibbles with the same number of rows. The first, called metrics, contains the downloaded
+#' publication meta data for the supplied DOIs. The second one, called api_info, provides data on the status of each
+#' API call. It has four columns:
+#' \itemize{
+#'   \item `api_response_code` The response code of the API
+#'   \item `api_response_msg` The description of the response code, which can either be an error message or the word 'Success'
+#'   \item `remaining_calls_this_hour` The number of remaining API calls beeing until the hourly rate limit is reached
+#'   \item `remaining_calls_today` The number of remaining API calls beeing until the daily rate limit is reached
+#' }
+#' @examples my_dois <- c("10.7191/jeslib.2021.1180", "10.1108/02640470610689151", ",,,not,a,doi,,,")
+#'
+#' result <- doi_get_altmetrics(my_dois)
+#' result
+#'
+#' # successful calls only
+#' result$metrics[result$api_info$api_response_msg == "Success", ]
+#'
+#' # failed calls only
+#' result$metrics[result$api_info$api_response_msg != "Success", ]
+#'
+#' # mean Altmetric score of all DOIs
+#' mean(result$metrics$score, na.rm = TRUE)
+#' @export
+doi_get_altmetrics <- function(doi_vec, api_key = NULL, wait_for = 1) {
+
+    # check validity of function arguments
+    argument_validity_check(doi_vec, api_key, wait_for)
+
+    # initialize progress bar
+    pb     <- utils::txtProgressBar(min = 0, max = length(doi_vec), style = 3)
+    # initialize return value
+    result <- list(list(metrics = tibble::tibble(), api_info = tibble::tibble()))
+
+    # loop over DOIs
+    for (i in seq_along(doi_vec)) {
+
+        # read metrics from API
+        result[[i]] <- doi_get_altmetrics_single(doi_vec[i], api_key[1])
+
+        # is a rate limit reached?
+        if ((dplyr::pull(result[[i]]$api_info, .data$api_response_code))[1] == 429) {
+
+            # is the daily or hourly limit reached? Warn accordingly
+            if ((dplyr::pull(result[[i]]$api_info, .data$remaining_calls_today))[1] == 0) {
+                warning("Daily rate limit reached. ", appendLF = FALSE)
+            } else {
+                warning("Hourly rate limit reached. ", appendLF = FALSE)
+            }
+
+            warning("Interrupting at this point. You only have a subset of the data you tried to retrieve.",
+                    "\nSee doi_get_altmetrics_background for easily handling the rate limit.")
+
+            # close the progress bar
+            close(pb)
+
+            # returning the intermediate result until this point
+            result[[i]] <- NULL
+            return(
+                list(metrics  = purrr::map_dfr(result, "metrics"),
+                     api_info = purrr::map_dfr(result, "api_info"))
+            )
+
+        }
+
+      # update progress bar
+      utils::setTxtProgressBar(pb, i)
+
+      # wait for the time specified in function argument
+      Sys.sleep(wait_for[1])
+
+    }
+
+    # close the progress bar
+    close(pb)
+
+    # message remaining API calls
+    if (is.null(api_key)) msg_limit_remainder(result[[i]]$api_info)
+
+    # return result
+    list(metrics  = purrr::map_dfr(result, "metrics"),
+         api_info = purrr::map_dfr(result, "api_info"))
+
+}
+
+
+
+
+#' @rdname doi_get_altmetrics
+#' @export
+#' @importFrom rlang .data
+doi_get_altmetrics_single <- function(doi_vec, api_key = NULL) {
+
+    # check validity of function arguments
+    argument_validity_check(doi_vec, api_key)
+
+    # create API call url and perform the request
+    ApiUrl   <- "https://api.altmetric.com/v1/doi/"
+    url      <- stringr::str_c(ApiUrl, doi_vec[1], ifelse(is.null(api_key[1]), "", stringr::str_c("?key=", api_key[1])))
+    response <- httr::GET(url)
+
+    # is the response a success or not? build metrics tibble and api_info tibble accordingly
+    if (response$status_code == 200) {
+        metrics  <- httr::content(response)
+        metrics  <- altmetric_response_to_tibble(metrics)
+        metrics  <- dplyr::rename(metrics, altmetric_doi = .data$doi)
+        metrics  <- dplyr::mutate(metrics, doi = doi_vec[1])
+        metrics  <- dplyr::select(metrics, .data$doi,dplyr::everything())
+        api_info <- tibble::tibble(api_response_msg  = "Success")
+    } else {
+        metrics  <- tibble::tibble(doi = doi_vec[1])
+        api_info <- tibble::tibble(api_response_msg  = altmetric_status_code_description(response$status_code))
+    }
+
+    # attach further info to api_tibble (response status code and remainig calls)
+    api_info <- dplyr::mutate(
+                    api_info,
+                    #api_response_code         = response$status_code,
+                    #remaining_calls_this_hour = response$headers$`x-hourlyratelimit-remaining`,
+                    #remaining_calls_today     = response$headers$`x-dailyratelimit-remaining`
+                    api_response_code         = 429,
+                    remaining_calls_this_hour = 0,
+                    remaining_calls_today     = 0
+    )
+
+    # return result
+    list(metrics = metrics, api_info = api_info)
+
+}
+
+
+
+
+
+
+
+
+#' @rdname doi_get_altmetrics
+#' @export
+doi_get_altmetrics_background <- function(doi_vec, api_key = NULL) {
+
+    # check validity of function arguments
+    argument_validity_check(doi_vec, api_key)
+
+    # initialize return value
+    result         <- list(list(metrics = tibble::tibble(), api_info = tibble::tibble()))
+    # backup file name for intermediate result in case process is being interrupted
+    backup_file    <- "altmetrics_backup.rds"
+    # status of the backup functionality
+    backup_working <- TRUE
+
+    # loop over DOIs
+    for (i in seq_along(doi_vec)) {
+
+        message("Trying DOI ", i, "/", length(doi_vec), " [ ", doi_vec[i], " ] ...")
+
+        # read metrics from API
+        result[[i]] <- doi_get_altmetrics_single(doi_vec[i], api_key[1])
+
+        # is a rate limit reached?
+        if ((dplyr::pull(result[[i]]$api_info, .data$api_response_code))[1] == 429) {
+
+            message("API rate limit reached. ", appendLF = FALSE)
+
+            # attempt to save intermediate result to file
+            if (backup_working) {
+
+                message("Saving intermediate result to ", backup_file, "... ", appendLF = FALSE)
+                a <- tryCatch(readr::write_rds(result, backup_file), error = function(e) a)
+                if (inherits(a, "error")) {
+                    message("\n   --->>> FAIL! <<<---\nCannot save intermediate result to file. Proceeding without backup.")
+                    backup_working <- FALSE
+                } else {
+                    message("Success.")
+                }
+            }
+
+            # is the daily or hourly limit reached? Wait accordingly
+            if ((dplyr::pull(result[[i]]$api_info, .data$remaining_calls_today))[1] == 0) {
+                message("Waiting a day to proceed. Continue download at ", Sys.time() + 60*60*24, "...")
+                #Sys.sleep(60*60*24)
+                Sys.sleep(8)
+            } else {
+                message("Waiting an hour to proceed. Continue download at ", Sys.time() + 60*60, "...")
+                Sys.sleep(60*60)
+            }
+
+            # attempt to restore intermediate result from file
+            if (backup_working) {
+
+                message("Loading intermediate result from ", backup_file, "... ", appendLF = FALSE)
+                a <- tryCatch(readr::read_rds(backup_file), error = function(e) e)
+                if (inherits(a, "error")) {
+                    message("\n   --->>> FAIL! <<<---\nCannot load intermediate result from file. Proceeding without backup.")
+                    backup_working <- FALSE
+                } else {
+                    message("Success.")
+                    result <- a
+                }
+
+            }
+
+            # decrementing iterator in order to repeat the last query that failed due to the rate limit
+            i <- i - 1
+
+        }
+
+        # wait one second between each call
+        Sys.sleep(1)
+
+    }
+
+    # message remaining API calls
+    if (is.null(api_key)) msg_limit_remainder(result[[i]]$api_info)
+
+    # return result
+    list(metrics  = purrr::map_dfr(result, "metrics"),
+         api_info = purrr::map_dfr(result, "api_info"))
+
+}
+
+
---
+++ R/doi.R
@@ -0,0 +1,170 @@
+#
+# A set of functions for handling DOIs (digital object identifiers)
+#
+
+
+
+
+
+
+# global regular expression that identifies a DOI
+pkg.env <- new.env()
+pkg.env$doi_regex <- "10\\.\\d{4,9}/[-._;()/:a-z0-9A-Z]+"
+
+
+
+
+
+#' Set the regular expression used to find a DOI
+#'
+#' The doi package uses a regular expression to find or extract DOIs.
+#' Its standard value is "10\\\\.\\\\d{4,9}/\[-._;()/:a-z0-9A-Z\]+".
+#' Use this function to set a custom regular expression of your choice, valid for the current session.
+#' Omit the argument to set the regular expression back to standard.
+#'
+#' @param rg_expr A single string containing the regular expression to use further on during a session.
+#' @export
+doi_set_regex <- function(rg_expr = "10\\.\\d{4,9}/[-._;()/:a-z0-9A-Z]+") {
+
+  if (!is.character(rg_expr)) stop("Argument rg_expr must be of type character.")
+  if (length(rg_expr) == 0)   stop("Argument rg_expr must not be of length 0.")
+  if (length(rg_expr) > 1)    warning("Length of argument rg_expr greater than 1. Only first element used.")
+  pkg.env$doi_regex <- rg_expr[1]
+
+}
+
+
+
+
+
+#' Turn DOIs into URLs
+#'
+#' Turns DOIs into URLs by placing the prefix "https://www.doi.org/" in front of them.
+#'
+#' @param doi_vec A character vector containing DOIs.
+#' @return A character vector containing URLs.
+#' @export
+doi_to_url <- function(doi_vec) {
+
+  if (!is.character(doi_vec)) stop("Argument doi_vec must be of type character.")
+  if (length(doi_vec) == 0)   return(character(0))
+  stringr::str_c("https://www.doi.org/", doi_vec)
+
+}
+
+
+
+
+
+#' Detect the presence or absence of a DOI in a string
+#'
+#' Returns TRUE if a given string contains a valid DOI. Vectorised over doi_vec.
+#'
+#' @param doi_vec A character vector containing DOIs.
+#' @param na_yields_false A single logical value stating how to deal with NAs in the input vector.
+#' Set to FALSE, doi_detect will return NA. Set to TRUE, it will return FALSE.
+#' @return A logical vector with TRUE indicating whether a DOI was found.
+#' @export
+doi_detect <- function(doi_vec, na_yields_false = FALSE) {
+
+  if (!is.character(doi_vec))       stop("Argument doi_vec must be a character vector.")
+  if (!is.logical(na_yields_false)) stop("Argument na_yields_false must be of type logical.")
+  if (length(na_yields_false) == 0) stop("Argument na_yields_false must not be of length 0.")
+  if (length(na_yields_false) > 1)  warning("Length of argument na_yields_false greater than 1. Only first element used.")
+  doi_vec <- tidyr::replace_na(doi_vec, NA_character_)
+
+  a   <- stringr::str_detect(doi_vec, pkg.env$doi_regex)
+  if (na_yields_false[1]) tidyr::replace_na(a, replace = FALSE) else a
+
+}
+
+
+
+
+
+#' Check if a string is a DOI
+#'
+#' Returns TRUE if a given string is a valid DOI. Vectorised over doi_vec.
+#'
+#' @param doi_vec A character vector containing DOIs.
+#' @param na_yields_false A single logical value stating how to deal with NAs in the input vector.
+#' Set to FALSE, doi_check will return NA. Set to TRUE, it will return FALSE.
+#' @return A logical vector with TRUE indicating whether an element of the input vector is a valid DOI.
+#' @export
+doi_check <- function(doi_vec, na_yields_false = FALSE) {
+
+  if (!is.character(doi_vec))       stop("Argument doi_vec must be a character vector.")
+  if (!is.logical(na_yields_false)) stop("Argument na_yields_false must be of type logical.")
+  if (length(na_yields_false) == 0) stop("Argument na_yields_false must not be of length 0.")
+  if (length(na_yields_false) > 1)  warning("Length of argument na_yields_false greater than 1. Only first element used.")
+  doi_vec <- tidyr::replace_na(doi_vec, NA_character_)
+
+  rgx <- stringr::str_c("^", pkg.env$doi_regex, "$")
+  a   <- stringr::str_detect(doi_vec, rgx)
+  if (na_yields_false[1]) tidyr::replace_na(a, replace = FALSE) else a
+
+}
+
+
+
+
+
+#' Extracts the first DOI from a string
+#'
+#' Extracts valid DOIs from a given character vector. Vectorised over doi_vec.
+#' Only returns the first match for each element.
+#'
+#' @param doi_vec A character vector.
+#' @param lower_case A single logical value stating whether or not to put the whole DOI in lower case.
+#' As DOIs are case insensitive, using lower case for all may help to identify duplicates more easily.
+#' @return A character vector containing DOIs (if found) or NAs otherwise.
+#' @export
+doi_extract_first <- function(doi_vec, lower_case = FALSE) {
+
+  if (!is.character(doi_vec))  stop("Argument doi_vec must be a character vector.")
+  if (!is.logical(lower_case)) stop("Argument lower_case must be of type logical.")
+  if (length(lower_case) == 0) stop("Argument lower_case must not be of length 0.")
+  if (length(lower_case) > 1)  warning("Length of argument lower_case greater than 1. Only first element used.")
+  doi_vec <- tidyr::replace_na(doi_vec, NA_character_)
+
+  a <- stringr::str_extract(doi_vec, pkg.env$doi_regex)
+  if (lower_case[1]) tolower(a) else a
+
+}
+
+
+
+
+
+#' Extracts all DOIs from a string
+#'
+#' Extracts all valid DOIs from a given character vector. Vectorised over doi_vec.
+#'
+#' @param doi_vec A character vector.
+#' @param lower_case A single logical value stating whether or not to put the whole DOI in lower case.
+#' As DOIs are case insensitive, using lower case for all may help to identify duplicates more easily.
+#' @param simplify A single logical value stating whether or not to flatten the resulting list into
+#' a character vector with unique DOIs only. This is useful if you just want to retrieve all unique
+#' DOIs from a text and you do not mind loosing the mapping from the input vector to the output.
+#' @return A list of character vectors containing DOIs (if found) or NAs otherwise. The length of the
+#' list equals the length of the input vector. In case simplify = TRUE, a single character vector is
+#' returned, whose length is not related to the length of the input vector.
+#' @export
+doi_extract_all <- function(doi_vec, lower_case = FALSE, simplify = FALSE) {
+
+  if (!is.character(doi_vec))  stop("Argument doi_vec must be a character vector.")
+  if (!is.logical(lower_case)) stop("Argument lower_case must be of type logical.")
+  if (length(lower_case) == 0) stop("Argument lower_case must not be of length 0.")
+  if (length(lower_case) > 1)  warning("Length of argument lower_case greater than 1. Only first element used.")
+  if (!is.logical(simplify))    stop("Argument simplify must be of type logical.")
+  if (length(simplify) == 0)    stop("Argument simplify must not be of length 0.")
+  if (length(simplify) > 1)     warning("Length of argument simplify greater than 1. Only first element used.")
+  doi_vec <- tidyr::replace_na(doi_vec, NA_character_)
+
+  a <- stringr::str_extract_all(doi_vec, pkg.env$doi_regex)
+  if (lower_case[1]) a <- purrr::map(a, ~ tolower(.x))
+  if (simplify[1]) unique(purrr::flatten_chr(a)) else a
+
+}
+
+
